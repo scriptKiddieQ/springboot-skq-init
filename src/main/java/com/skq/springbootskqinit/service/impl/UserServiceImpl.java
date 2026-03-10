f976f0c98d38101031c4448a26863966489ecbf0
qwe <@>
2026-03-10 13:59:44 +0800

    2.项目初始化 1.引入通用响应类 BaseResponse 2.删除请求封装类 DeleteRequest 3.分页请求封装类 PageRequest 4.通用响应结果工具类 ResultUtils 5.全局跨域配置 CorsConfig 6.JsonConfig 解决 json 精度丢失的配置 7.MyBatisPlus配置类 MyBatisPlusConfig 8.自定义抛异常工具类 ThrowUtils 9.自定义业务异常类 BusinessException 10.统一的错误码枚举 ErrorCode 11.全局异常处理 GlobalExceptionHandler

--- src/main/java/com/skq/springbootskqinit/service/impl/UserServiceImpl.java
+++ src/main/java/com/skq/springbootskqinit/service/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.skq.springbootskqinit.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.skq.springbootskqinit.exception.BusinessException;
+import com.skq.springbootskqinit.exception.ErrorCode;
+import com.skq.springbootskqinit.exception.ThrowUtils;
 import com.skq.springbootskqinit.model.entity.User;
 import com.skq.springbootskqinit.service.UserService;
 import com.skq.springbootskqinit.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
